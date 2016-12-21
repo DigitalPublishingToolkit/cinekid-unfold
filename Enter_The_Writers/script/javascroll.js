b75aabb78edc8d67692128ca3257f29cbaa370b7
Lucia Dossin <lucia@fiveblackcats.com>
2016-12-21 12:29:22 +0100

    pong fix

--- Enter_The_Writers/script/javascroll.js
+++ Enter_The_Writers/script/javascroll.js
@@ -1,76 +1,33 @@
-(function() {
-  // retrieved from http://stackoverflow.com/questions/34297214/inverse-scroll-two-divs and adapted
+(function () {
+    // retrieved from http://stackoverflow.com/questions/34297214/inverse-scroll-two-divs and adapted
 
-  $(window).load(function(){
+    $(window).load(function () {
 
-  // Start off scrolled to the bottom
-  var $selects = $('.scroll2');
+        // Start off scrolled to the bottom
+        var $divs = $('.scroll2');
 
-  $selects.last().scrollTop($selects.last().get(0).scrollHeight);
+        $divs.last().scrollTop($divs.last().get(0).scrollHeight);
 
-  // console.log('should go to '+ $selects.last().get(0).scrollHeight);
-  // console.log('first elem ' + $selects.first().get(0).scrollHeight)
+        // console.log('should go to '+ $selects.last().get(0).scrollHeight);
+        // console.log('first elem ' + $selects.first().get(0).scrollHeight)
 
-  var lastScroll = {
-    whichEle: null,
-    position: 0
-  };
 
-  // Wait until the scrollTop has applied itself (aka a repaint)
-  window.requestAnimationFrame(function() {
-    $selects.on('scroll', scrollOther);
-  });
+        var sync = function (e) {
 
-  function scrollOther() {
-    var $other = $selects.not(this);
+            var $other = $divs.not(this).off('scroll'),
+                other = $other.get(0);
 
-    var currPosition = $(this).scrollTop() ;
-    // var maxPosition = $(this).get(0).scrollHeight;
-    // var maxPositionOther = $other.get(0).scrollHeight;
-    // var factor = maxPosition / maxPositionOther; //proportion between 2 heights
-    // console.log('factor '+ factor);
+            //console.log($other.get(0)["id"], this);
 
-    if ($(this).is(lastScroll.whichEle)) {
-      var positionDiff = currPosition - lastScroll.position;
+            var percentage = this.scrollTop / (this.scrollHeight - this.offsetHeight);
+            other.scrollTop = (1 - percentage) * (other.scrollHeight - other.offsetHeight);
 
-      var maxPosition = $(this).get(0).scrollHeight;
-      var maxPositionOther = $other.get(0).scrollHeight;
-      var factor = maxPosition / maxPositionOther; //proportion between 2 heights
-      console.log('factor '+ factor);
+            setTimeout(function () {
+                $other.on('scroll', sync);
+            }, 10);
+        }
+        $divs.on('scroll', sync);
 
-      console.log('max ' + maxPosition );
-      console.log('maxOther ' + maxPositionOther );
-      console.log('perc ' + currPosition / maxPosition * 100);
-      //console.log('other perc ' +  (currPosition / maxPositionOther * 100) / factor);
-
-      if(positionDiff === 0) { return; }
-
-      // var $other = $selects.not(this); //moved to outside the function
-
-      $other.off('scroll'); // temporarily stop listening
-
-      // Wait until the handler released itself (aka a repaint)
-      window.requestAnimationFrame(function() {
-        // Since scroll events happen so frequently, no need to animate (unless there's a debounce)
-          //$other.scrollTop($other.scrollTop() - positionDiff);
-          $other.scrollTop($other.scrollTop() - (positionDiff / factor));
-          console.log('scrolling '+ (positionDiff / factor));
-
-        // Wait until the scrollTop has applied itself (aka a repaint)
-        window.requestAnimationFrame(function() {
-          $other.on('scroll', scrollOther); // restart listening
-        });
-      });
-    } else {
-      // Just started scrolling a new div
-      lastScroll.whichEle = $(this);
-
-      // Since there is no direction figured out yet, do nothing more than record for this event
-    }
-
-    lastScroll.position = currPosition;
-  }
-
-  });
+    });
 
 })();
